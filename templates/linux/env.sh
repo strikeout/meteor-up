e74a3bffea95a766cdf26b6a510803951a51e8ef
Thomas Steinhauer <thomassteinhauer@gmail.com>
2015-01-28 10:32:49 +0100

    working automatic multi-core configuration

--- templates/linux/env.sh
+++ templates/linux/env.sh
@@ -2,9 +2,13 @@
 export PORT=80
 export MONGO_URL=mongodb://127.0.0.1/<%= appName %>
 export ROOT_URL=http://localhost
-export CORES="grep -c ^processor /proc/cpuinfo"
 
 #it is possible to override above env-vars from the user-provided values
 <% for(var key in env) { %>
   export <%- key %>=<%- ("" + env[key]).replace(/./ig, '\\$&') %>
 <% } %>
+
+
+#put number of cores in nginx conf
+export CORES=$(grep -c ^processor /proc/cpuinfo)
+sed -i -e 's/CORES/'"$CORES"'/g' /etc/nginx/sites-enabled/<%= appName %>
